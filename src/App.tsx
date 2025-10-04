8896764eb1c83dff7344e6d4a9e7c01d011447c9
gustavo-mercier <galexmercier@gmail.com>
2025-10-03 22:28:43 -0500

    Fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,34 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { useJsonQuery } from './utilities/fetch';
 import Banner from "./components/Banner";
 import CourseList from "./components/CourseList";
 
 // Put Banner and CourseList components into their own files in src/components and import into App.tsx.
 // Pass the courses object to CourseList. 
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
+interface course {
+  term: string,
+  number: string,
+  meets: string,
+  title: string
+}
+
+interface courseList {
+  title: String,
+  courses: Record<string, course>
+}
 
 const App = () => {
+  const [json, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
+
+  if (error) return <h1>Error loading user data: {`${error}`}</h1>;
+  if (isLoading) return <h1>Loading user data...</h1>;
+  if (!json) return <h1>No user data found</h1>;
+
+  const schedule = json as courseList;
+
   return (
     <div className="bg-[#282c34] min-h-screen flex flex-col text-[calc(10px_+_2vmin)] text-white p-4">
       <Banner title={schedule.title}/>
